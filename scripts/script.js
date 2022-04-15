6920f28070397fcaa7a1b031979e7d42d3464efc
PauloGodoiS <paulogodoi.sp@gmail.com>
2022-04-14 22:52:14 -0300

    Slide completo

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,35 @@
 let count = 1
-document.getElementById("radio1").checked = true 
+document.getElementById("radio1").checked = true
+
+let nameTennis = document.getElementById("nameTennis")
+nameTennis.innerHTML = 'ULTRABOOST 5.0 DNA'
+
+let radio4 = document.getElementById("radio4").checked
+
 
 setInterval( function() {
     nextImage()
-}, 3000)
+}, 5000)
+
+function onclick1() {
+    radio4.checked = true
+    nameTennis.innerHTML = 'ULTRABOOST 5.0 DNA'
+}
+
+function onclick2() {
+    radio4.checked = true
+    nameTennis.innerHTML = 'GRAND COURT BASE'
+}
+
+function onclick3() {
+    radio4.checked = true
+    nameTennis.innerHTML = 'D-Formation'
+}
+
+function onclick4() {
+    radio4.checked = true
+    nameTennis.innerHTML = 'Pegasus Trail 3'
+}
 
 function nextImage() {
     count++
@@ -11,6 +37,18 @@ function nextImage() {
         count = 1
     }
 
-    document.getElementById("radio"+ count).checked = true 
+    document.getElementById("radio"+ count).checked = true
 
+    if(count === 1) {
+        nameTennis.innerHTML = 'ULTRABOOST 5.0 DNA'
+    }
+    if(count === 2) {
+        nameTennis.innerHTML = 'GRAND COURT BASE'
+    }
+    if(count === 3) {
+        nameTennis.innerHTML = 'D-Formation'
+    }
+    if(count === 4) {
+        nameTennis.innerHTML = 'Pegasus Trail 3'
+    }
 }
